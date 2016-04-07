4ae8168a042892ba5c9c0da0f67939fc8491bca8
oss6 <ossedb@gmail.com>
2016-04-07 03:38:48 +0100

    Changes

---
+++ test/writer.js
@@ -0,0 +1,12 @@
+'use strict';
+
+//var assert = require('assert');
+var writer = require('../lib/writer');
+
+describe('writer', function () {
+
+    describe('createProject', function () {
+        writer.createProject();
+    });
+
+});
