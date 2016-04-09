278ec985e3f9aa93e64c36d4dc336fb53caa7173
oss6 <ossedb@gmail.com>
2016-04-09 01:25:17 +0100

    Added tests for writer

--- test/writer.js
+++ test/writer.js
@@ -1,12 +1,46 @@
 'use strict';
-
-//var assert = require('assert');
+var assert = require('assert');
+var fs = require('fs-extra');
+var path = require('path');
 var writer = require('../lib/writer');
 
 describe('writer', function () {
 
+    var paths = [
+        'settings.json',
+        'static',
+        'templates',
+        'contents'
+    ];
+
+    var checkExistsAll = function (cb) {
+        var toGo = paths.length;
+
+        paths.forEach(function (p) {
+            fs.stat(path.join('.', p), function (err) {
+                if (err) {
+                    if (err.code === 'ENOENT') {
+                        assert(false, 'The file should exist');
+                    }
+                } else {
+                    assert(true);
+                }
+
+                if (--toGo === 0) {
+                    cb();
+                }
+            });
+        });
+    };
+
     describe('createProject', function () {
-        writer.createProject();
+
+        writer.createProject(function () {
+            checkExistsAll(function () {
+                writer.clearWorkspace();
+            });
+        });
+
     });
 
 });
