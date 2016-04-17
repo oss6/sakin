fdf784c34f6984f44bb84d62eef3fe66700a9cb3
oss6 <ossedb@gmail.com>
2016-04-17 17:27:53 +0100

    :mag: test: added saveToOutput
    Added saveToOutput test

--- test/writer.js
+++ test/writer.js
@@ -2,6 +2,7 @@
 var assert = require('assert');
 var fs = require('fs-extra');
 var path = require('path');
+var a = require('async');
 var writer = require('../lib/writer');
 var ActionObservable = require('../lib/action-observable');
 
@@ -14,10 +15,10 @@ describe('writer', function () {
         'contents'
     ];
 
-    var checkExistence = function (checkExists, cb) {
-        var toGo = paths.length;
+    var checkExistence = function (checkExists, ps, cb) {
+        var toGo = ps.length;
 
-        paths.forEach(function (p) {
+        ps.forEach(function (p) {
             fs.stat(path.join('.', p), function (err) {
                 if (err) {
                     if (err.code === 'ENOENT') {
@@ -39,7 +40,7 @@ describe('writer', function () {
     describe('createProject', function () {
         it('should create all the necessary files', function (done) {
             writer.createProject(action, function () {
-                checkExistence(true, function () {
+                checkExistence(true, paths, function () {
                     writer.clearWorkspace(action, done);
                 });
             });
@@ -50,14 +51,34 @@ describe('writer', function () {
         it('should clear all the created files', function (done) {
             writer.createProject(action, function () {
                 writer.clearWorkspace(action, function () {
-                    checkExistence(false, done);
+                    checkExistence(false, paths, done);
                 });
             });
         });
     });
 
     describe('saveToOutput', function () {
+        it('should create the output files as defined in the parameter', function (done) {
+            var files = [
+                {path: path.join('output', 'about.html'), content: '<h1>Hello</h1>'},
+                {path: path.join('output', 'contacts.html'), content: '<h1>How are you?</h1>'}
+            ];
 
+            var ps = files.map(function (file) {
+                return file.path;
+            });
+
+            // Create files
+            a.each(files, function (file, cb) {
+                fs.outputFile(file.path, file.content, cb);
+            }, function () {
+                writer.saveToOutput(action, files, function () {
+                    checkExistence(true, ps, function () {
+                        writer.clearWorkspace(action, done);
+                    });
+                });
+            });
+        });
     });
 
     describe('createFile', function () {
