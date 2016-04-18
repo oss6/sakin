933606acf71d8770d304de71fa196ccd280ac15f
oss6 <ossedb@gmail.com>
2016-04-18 02:08:03 +0100

    :wrench: fix: gulpfile pretest
    Add to exclude files cli.js and cli-theme.js

--- gulpfile.js
+++ gulpfile.js
@@ -22,7 +22,7 @@ gulp.task('nsp', function (cb) {
 });
 
 gulp.task('pre-test', function () {
-  return gulp.src(['lib/**/*.js', '!lib/data/**'])
+  return gulp.src(['lib/**/*.js', '!lib/data/**', '!lib/cli-theme.js', '!lib/cli.js'])
     .pipe(istanbul({
       includeUntested: true
     }))
